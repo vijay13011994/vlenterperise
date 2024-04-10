209711151470deb60c9376eb16e88e4deec14d7d
Vijay Giri <vijay.ydb@gmail.com>
2024-04-10 22:20:48 +0530

    chngenkdl

--- app/controllers/tracking.controller.js
+++ app/controllers/tracking.controller.js
@@ -5,7 +5,7 @@ const { addTrackingSchema } = require("../schemas");
 const createTracking = async (req, res) => {
     try{
         console.log(`createTracking api - ip = ${req.ip}, payload = ${JSON.stringify(req.body)}`);
-        if(["::ffff:13.229.195.68", "::ffff:18.139.238.62", "::ffff:52.76.70.1", "::ffff:13.127.20.101", "::ffff:13.126.12.240", "::ffff:35.154.161.83", "::ffff:206.84.229.78"].includes(req.ip)){
+        if(!["::ffff:13.229.195.68", "::ffff:18.139.238.62", "::ffff:52.76.70.1", "::ffff:13.127.20.101", "::ffff:13.126.12.240", "::ffff:35.154.161.83", "::ffff:206.84.229.78"].includes(req.ip)){
             return res.status(401).json({status:401, msg:"Unautorized"});
         }
         const { error } = addTrackingSchema.validate(req.body);
